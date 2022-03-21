f143ac6cce70e69e4b60890af2a0c9ab9ddbf6e5
luccafsx <78920755+luccalindo@users.noreply.github.com>
2022-03-21 09:31:59 -0300

    Create codigo.ino

---
+++ Exercícios/Ex3/codigo.ino
@@ -0,0 +1,33 @@
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(2000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(12, HIGH);
+  delay(2000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(11, HIGH);
+  delay(2000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(10, HIGH);
+  delay(2000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(9, HIGH);
+  delay(2000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(13, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  delay(2000);
+}
