9cdcc19093e7693fface2d70d8a17e313934b72e
luccafsx <78920755+luccalindo@users.noreply.github.com>
2022-03-21 17:37:25 -0300

    Update codigo.ino

--- Exercícios/Ex2/codigo.ino
+++ Exercícios/Ex2/codigo.ino
@@ -1,20 +1,17 @@
-// C++ code
-//
 void setup()
 {
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(10, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(10, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
   
-  digitalWrite(11, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(11, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+digitalWrite(11,HIGH);
+  digitalWrite(10,LOW);
+  delay(1000);
+  
+  digitalWrite(10,HIGH);
+  digitalWrite(11,LOW);
+  delay(1000);
 }
